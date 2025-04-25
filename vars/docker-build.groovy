bf2519c890b27a0228f02db463025e6f2b6e4b2a
saurabh2nd <saurabhpandey689@gmail.com>
2025-04-25 12:53:40 +0530

    Update docker-build.groovy

--- vars/docker-build.groovy
+++ vars/docker-build.groovy
@@ -1,4 +1,4 @@
-def call(String ProjectName, String ImageTag){
+def call( String ProjectName, String ImageTag){
   echo "this is building of image"
   sh "docker build -t ${ProjectName}:${ImageTag} ."
   echo "image build successfully"
